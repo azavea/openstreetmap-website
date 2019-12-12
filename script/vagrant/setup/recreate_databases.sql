29a92bee6b33dee460be9b66ba1d19515f7cb625
Kathryn Killebrew <kkillebrew@azavea.com>
2019-12-12 14:35:56 -0500

    Create separate test database user
    Fixes development database owner not having sufficient permissions for testing.

--- script/vagrant/setup/recreate_databases.sql
+++ script/vagrant/setup/recreate_databases.sql
@@ -6,10 +6,12 @@ DROP ROLE IF EXISTS openstreetmap;
 DROP ROLE IF EXISTS vagrant;
 
 CREATE ROLE vagrant SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;
+CREATE ROLE osm_test WITH PASSWORD 'osm_test' SUPERUSER CREATEDB INHERIT LOGIN;
 CREATE ROLE openstreetmap WITH PASSWORD 'openstreetmap' LOGIN;
 
 CREATE DATABASE vagrant WITH OWNER vagrant;
 CREATE DATABASE openstreetmap WITH OWNER openstreetmap;
-CREATE DATABASE osm_test WITH OWNER openstreetmap;
+CREATE DATABASE osm_test WITH OWNER osm_test;
 
 GRANT openstreetmap TO vagrant;
+GRANT osm_test TO vagrant;
